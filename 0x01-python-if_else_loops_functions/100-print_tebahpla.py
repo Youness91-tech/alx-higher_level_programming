838b2810f52b5243e401a99b051d48c74ed07a47
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-10-03 13:14:36 -0700

    100-print_tebahpla.py updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for char in range(122, 96, -1):
-    print("{:c}".format(char - 32) 
-            if ch % 2 != 0 
-            else "{:c}".format(char), end="")
+i = 0
+for ch in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(ch - i)), end="")
+    i = 32 if i == 0 else 0
+
