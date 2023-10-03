aa22cc129558c14e5803c34cbe7df2d0aa8cfe28
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-10-03 13:08:44 -0700

    100-print_tebahpla.py updtd

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for char in range(122, 96, -1):
-    print("{:c}".format(char - 32) if ch % 2 != 0 else "{:c}".format(char), end="")
+    print("{:c}".format(char - 32) 
+            if ch % 2 != 0 
+            else "{:c}".format(char), end="")
