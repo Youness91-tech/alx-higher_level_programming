b9b4ddc7e2538c4f1ca8b25282dd16c1c4413f9c
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-10-11 12:55:27 -0700

    12-roman_to_int.py updated

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,9 @@ def roman_to_int(roman_string):
         return 0
     roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    for value in range(len(roman_string)):
-        if value > 0 and roman_values[roman_string[value]] > roman_values[roman_string[value - 1]]:
-            total += roman_values[roman_string[value]] - 2 * roman_values[roman_string[value - 1]]
+    for v in range(len(roman_string)):
+        if v > 0 and roman_values[roman_string[v]] > roman_values[roman_string[v - 1]]:
+            total += roman_values[roman_string[v]] - 2 * roman_values[roman_string[v - 1]]
         else:
-            total += roman_values[roman_string[value]]
+            total += roman_values[roman_string[v]]
     return total
