85482351cc92fe58ac116c8b069ea544e255e79e
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-10-23 14:21:50 -0700

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for idx in range(0, x):
+        try:
+            print("{:d}".format(my_list[idx]), end="")
+            counter += 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return counter
