06d16c02a5d6f917012ffe6c565b3bc34cf41753
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-10-24 07:42:34 -0700

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    Defines properties of square.
+
+    Attributes:
+        size: size of square.
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """New instances of square.
+
+        Args:
+            __size (int): size of the square (1 side).
+            __position (tuple): position of the square.
+        """
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Calculates the area of square.
+
+        Returns: the current square area.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Returns the size of a square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer.
+            ValueError: size must be >= 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter for the position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter for position.
+
+        Args:
+            value (tuple): position of the square.
+
+        Raises:
+            TypeError: must be a tuple of 2 positive integers
+        """
+        if not isinstance(value, tuple):
+            raise TypeError("must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """prints the square
+        """
+
+        if self.__size == 0:
+            print()
+        else:
+            for j in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(" ",  end="")
+                print("#" * (self.__size))
