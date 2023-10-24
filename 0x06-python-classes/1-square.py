4cd2c37e751a8b060a2fd6c8431b74ef30a8d89d
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-10-24 07:18:50 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines a square"""
+
+
+class Square:
+    """ Defines a square"""
+
+    def __init__(self, size):
+        """initialization of square
+        
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
