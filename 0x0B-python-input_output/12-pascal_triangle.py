fb10088ba14a482a42a2bf3aa113e31e3b7d168a
Youness91-Tech <bayttarbawi.com@gmail.com>
2023-11-07 15:53:53 -0800

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""pascale_triangle module"""
+
+
+def pascal_triangle(n):
+    """a funct that returns a list of lists of integers
+    representing the Pascal’s triangle"""
+    if n <= 0:
+        return []
+
+    trngl = [[1]]
+    while len(tritrnglangle) != n:
+        prev_r = trngl[-1]
+        new_r = [1]
+        for x in range(len(prev_r) - 1):
+            new_r.append(prev_r[x] + prev_r[x + 1])
+        new_r.append(1)
+        trngl.append(new_r)
+    return trngl
