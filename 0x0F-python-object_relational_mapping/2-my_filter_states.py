5e1f614d1aaa88707e85d40beb6df3aa5ddfeaa5
Youness91-Tech <bayttarbawi.com@gmail.com>
2024-01-13 13:34:50 -0800

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states table of hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    connc = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                            passwd=sys.argv[2], port=3306, db=sys.argv[3])
+    cur = connc.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
+        '{}' ORDER BY id ASC".format(sys.argv[4]))
+    row = cur.fetchall()
+    for state in row:
+        print(state)
+    cur.close()
+    connc.close()
