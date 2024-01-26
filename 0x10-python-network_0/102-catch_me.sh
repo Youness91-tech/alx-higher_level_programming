7109e0c3e3a586925127e3bb0b4b3ffa15deae78
Youness91-Tech <bayttarbawi.com@gmail.com>
2024-01-26 15:34:51 -0800

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-# Script that makes a request to 0.0.0.0:5000/catch_me 
-# that causes the server to respond with a message containing You got me!, in the body of the response.
+# script that makes a request to 0.0.0.0:5000/catch_me
 curl -s -L 0.0.0.0:5000/catch_me -d "user_id=98" -X PUT -H "Origin:School"
